bfab04442038b6e34e91f3066d1686af3b8ea7e5
Allan Jardine <allan@datatables.net>
2022-10-10 19:49:09 +0000

    Add support for mjs files (ES Modules)

---
+++ js/fixedColumns.bulma.mjs
@@ -0,0 +1,13 @@
+
+/*! Bulma integration for DataTables' FixedColumns
+ * © SpryMedia Ltd - datatables.net/license
+ */
+
+import $ from 'jquery';
+import DataTable from 'datatables.net-bm';
+import DataTable from 'datatables.net-fixedcolumns';
+
+
+
+
+export default DataTable;
