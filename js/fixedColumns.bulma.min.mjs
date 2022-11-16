25cc2bcb9b487304ed4dc1b77a89ae8075a5692a
Allan Jardine <allan.jardine@sprymedia.co.uk>
2022-11-16 17:06:37 +0000

    7bc6dcd5053b2d6128798325bbdf202dc14ef38c
    Sync to source repo @bc6dcd5053b2d6128798325bbdf202dc14ef38c

--- js/fixedColumns.bulma.min.mjs
+++ js/fixedColumns.bulma.min.mjs
@@ -1,4 +1,4 @@
 /*! Bulma integration for DataTables' FixedColumns
  * © SpryMedia Ltd - datatables.net/license
  */
-import $ from"jquery";import DataTable from"datatables.net-bm";import"datatables.net-fixedcolumns";export default DataTable;
+import $ from"jquery";import DataTable from"datatables.net-bm";import FixedColumns from"datatables.net-fixedcolumns";export default DataTable;
--- js/fixedColumns.bulma.mjs
+++ js/fixedColumns.bulma.mjs
@@ -4,7 +4,7 @@
 
 import $ from 'jquery';
 import DataTable from 'datatables.net-bm';
-import 'datatables.net-fixedcolumns';
+import FixedColumns from 'datatables.net-fixedcolumns';
 
 
 
