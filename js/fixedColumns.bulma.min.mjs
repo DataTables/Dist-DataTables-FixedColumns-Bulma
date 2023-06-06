c7c5d10fb71a96d22dff08bb114c2f29a81fbf67
Allan Jardine <allan.jardine@sprymedia.co.uk>
2023-06-06 11:06:26 +0100

    cad68434135f57d4d30f74d27b9e9dfaed9c4894 Fix: Memory leak as event listeners we not being removed
    https://datatables.net/forums/discussion/76449
    
    Sync to source repo @cad68434135f57d4d30f74d27b9e9dfaed9c4894

--- js/fixedColumns.bulma.min.mjs
+++ js/fixedColumns.bulma.min.mjs
@@ -1,4 +1,4 @@
 /*! Bulma integration for DataTables' FixedColumns
  * © SpryMedia Ltd - datatables.net/license
  */
-import $ from"jquery";import DataTable from"datatables.net-bm";import FixedColumns from"datatables.net-fixedcolumns";export default DataTable;
+import jQuery from"jquery";import DataTable from"datatables.net-bm";import FixedColumns from"datatables.net-fixedcolumns";let $=jQuery;export default DataTable;
