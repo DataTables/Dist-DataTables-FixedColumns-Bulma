21401a33c3c007cb471e99b991454f030e39caaa
dtbuild <dtbuild@rigil>
2026-05-07 13:46:53 +0000

    7f54158010ab3a53a3e0f1a9d806fc62bed20d3a
    Sync to source repo @f5b5ec8066104d02302d5f759b82044edfc080b7

--- js/fixedColumns.bulma.min.mjs
+++ js/fixedColumns.bulma.min.mjs
@@ -1,4 +1,4 @@
-/*! Bulma integration for DataTables' FixedColumns
- * © SpryMedia Ltd - datatables.net/license
+/*! FixedColumns Bulma styling 6.0.0-beta.1 for DataTables
+ * Copyright (c) SpryMedia Ltd - datatables.net/license
  */
-import jQuery from"jquery";import DataTable from"datatables.net-bm";import FixedColumns from"datatables.net-fixedcolumns";let $=jQuery;export default DataTable;
+import DataTable from"datatables.net-bm";import FixedColumns from"datatables.net-fixedcolumns";export default DataTable;
--- js/fixedColumns.bulma.mjs
+++ js/fixedColumns.bulma.mjs
@@ -1,14 +1,11 @@
-/*! Bulma integration for DataTables' FixedColumns
- * © SpryMedia Ltd - datatables.net/license
+/*! FixedColumns Bulma styling 6.0.0-beta.1 for DataTables
+ * Copyright (c) SpryMedia Ltd - datatables.net/license
  */
 
-import jQuery from 'jquery';
 import DataTable from 'datatables.net-bm';
 import FixedColumns from 'datatables.net-fixedcolumns';
-
-// Allow reassignment of the $ variable
-let $ = jQuery;
 
 
 
 export default DataTable;
+
